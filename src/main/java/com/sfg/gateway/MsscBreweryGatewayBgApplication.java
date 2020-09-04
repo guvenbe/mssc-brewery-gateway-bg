768248f05bd93700443429fef145eed9ad4bd225
Bora Guven <guvenbe@gmail.com>
2020-09-03 23:23:20 -0400

    first commit

---
+++ src/main/java/com/sfg/gateway/MsscBreweryGatewayBgApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryGatewayBgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweryGatewayBgApplication.class, args);
+    }
+
+}
